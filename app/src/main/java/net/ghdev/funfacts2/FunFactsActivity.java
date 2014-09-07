0d119d1fc3f9e7088537187987d9fb1794ce9eb0
Glenn Harris <harris.glenn.jr@gmail.com>
2014-09-06 19:18:28 -0700

    about to randomize background colors

---
+++ app/src/main/java/net/ghdev/funfacts2/FunFactsActivity.java
@@ -0,0 +1,44 @@
+package net.ghdev.funfacts2;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import static android.graphics.Color.RED;
+
+
+public class FunFactsActivity extends Activity {
+
+    private FactBook mFactBook = new FactBook();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relLayout);
+
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                // Update the label with our dynamic fact
+                factLabel.setText(fact);
+                relativeLayout.setBackgroundColor(RED);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+    }
+
+
+}
